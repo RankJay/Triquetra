d5e8ffb825f3b833f0d4786e1a792a1581e6f93e
Jay Rank <rankjay0101@gmail.com>
2021-05-07 22:02:22 +0530

    Update keys.py

--- scripts/keys.py
+++ scripts/keys.py
@@ -2,7 +2,7 @@
 # This file is not meant to be used in the main script.
 # Instead, copy this file into keys.py and put your own
 # keys there.
-CONSUMER_KEY = 'B49o1hOPsG1cvkkKuOpzbJPLC'
-CONSUMER_SECRET = 'vknDsXx6fUBCYsu4bDH5QHqn5VUjaJOP9DnVzewL92Phy1qeHY'
-ACCESS_KEY = '1378210856693952514-znxvwW9EOWdUq1Lgg55wsIEb4xf92R'
-ACCESS_SECRET = '74BGy7kGfZTxLUyB84YMrrhMBgB1cxH7n3zYpwsJD5Akj'
+CONSUMER_KEY = NULL
+CONSUMER_SECRET = NULL
+ACCESS_KEY = NULL
+ACCESS_SECRET = NULL
